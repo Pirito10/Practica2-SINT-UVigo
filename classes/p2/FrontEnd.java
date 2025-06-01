d0bf3c3f5a9509af8e3b6b677ad10ee23452dc1f
Pirito10 <aaronriveiro@gmail.com>
2025-06-01 21:07:31 +0200

    create sendHTMLF2

--- classes/p2/FrontEnd.java
+++ classes/p2/FrontEnd.java
@@ -64,8 +64,41 @@ public class FrontEnd {
         out.println("</body></html>");
     }
 
-    public static void sendHTMLF2(HttpServletResponse res) throws IOException {
+    public static void sendHTMLF2(HttpServletResponse res, String lang,
+            ArrayList<Cast> listaCast) throws IOException {
+        res.setContentType("text/html");
+        res.setCharacterEncoding("utf-8");
+        PrintWriter out = res.getWriter();
 
+        out.println("<html>");
+        out.println("<head><title>Fase 2</title></head>");
+        out.println("<body>");
+
+        out.println("<h1>Servicio de información sobre películas</h1>");
+        out.println("<h2>Fase 2 (idioma = " + lang + ")</h2>");
+        out.println("<p>Selecciona un actor/actriz:</p>");
+        out.println("<ol>");
+
+        for (Cast c : listaCast) {
+            out.println("<li>Nombre = <a href='P2M?pphase=3&plang=" + lang + "&pidC=" +
+                    c.getId() + "'>"
+                    + c.getName() + "</a> --- idC=" + c.getId() + "</li>");
+        }
+
+        out.println("</ol>");
+
+        out.println("<form method='get' action='P2M'>");
+        out.println("<input type='hidden' name='pphase' value='0'>");
+        out.println("<input type='submit' value='Inicio'>");
+        out.println("</form>");
+
+        out.println("<form method='get' action='P2M'>");
+        out.println("<input type='hidden' name='pphase' value='1'>");
+        out.println("<input type='submit' value='Atrás'>");
+        out.println("</form>");
+
+        out.println("<p>Aarón Riveiro Vilar (2024-2025)</p>");
+        out.println("</body></html>");
     }
 
     public static void sendHTMLF3(HttpServletResponse res) throws IOException {
