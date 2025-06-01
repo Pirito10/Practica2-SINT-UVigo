f52a1831c80a5eb2f18667fb80e9a8ac71170d22
Pirito10 <aaronriveiro@gmail.com>
2025-06-02 00:02:15 +0200

    add comments to Movie class

--- classes/p2/Movie.java
+++ classes/p2/Movie.java
@@ -1,16 +1,19 @@
 package p2;
 
+// Clase que representa una película
 public class Movie {
     private String id;
     private String title;
     private String year;
 
+    // Constructor
     public Movie(String id, String title, String year) {
         this.id = id;
         this.title = title;
         this.year = year;
     }
 
+    // Getters
     public String getId() {
         return id;
     }
