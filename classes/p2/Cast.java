daae360459247a55d9a9aefb1ed07ca22cc134a1
Pirito10 <aaronriveiro@gmail.com>
2025-06-01 21:00:43 +0200

    create Cast class

---
+++ classes/p2/Cast.java
@@ -0,0 +1,25 @@
+package p2;
+
+public class Cast {
+    private String id;
+    private String name;
+    private String role;
+
+    public Cast(String id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
