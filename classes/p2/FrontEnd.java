07c276c583ca38fea84ed44a5f43ec601d38dac9
Pirito10 <aaronriveiro@gmail.com>
2025-06-01 03:25:32 +0200

    refactor frontend class

--- classes/p2/FrontEnd.java
+++ classes/p2/FrontEnd.java
@@ -4,26 +4,43 @@ import java.io.*;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontEnd {
-    public static void sendHTMLF0(HttpServletResponse response, String resourceUrl, String clientIp)
+    public static void sendHTMLF0(HttpServletResponse res, String URL, String IP)
             throws IOException {
-        response.setContentType("text/html");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
+        res.setContentType("text/html");
+        res.setCharacterEncoding("utf-8");
+        PrintWriter out = res.getWriter();
 
         out.println("<html>");
         out.println("<head><title>Fase 0</title></head>");
         out.println("<body>");
 
-        out.println("<h2>Bienvenido al catálogo de películas</h2>");
-        out.println("<p>Fichero procesado: <a href=\"" + resourceUrl + "\">" + resourceUrl + "</a></p>");
-        out.println("<p>Tu IP es: " + clientIp + "</p>");
+        out.println("<h1>Servicio de información sobre películas</h1>");
+        out.println("<p>Fichero procesado: <a href=\"" + URL + "\">" + URL + "</a></p>");
+        out.println("<p>IP: " + IP + "</p>");
 
         out.println("<p id='navegador'></p>");
         out.println("<script>");
         out.println("document.getElementById('navegador').innerHTML = 'Navegador: ' + navigator.userAgent;");
         out.println("</script>");
 
-        out.println("<p>Autor: Aarón</p>");
+        out.println("<form method='get' action='P2M'>");
+        out.println("<input type='hidden' name='pphase' value='1'>");
+        out.println("<input type='submit' value='Siguiente'>");
+        out.println("</form>");
+
+        out.println("<p>Aarón Riveiro Vilar (2024-2025)</p>");
         out.println("</body></html>");
+    }
+
+    public static void sendHTMLF1(HttpServletResponse res) throws IOException {
+
+    }
+
+    public static void sendHTMLF2(HttpServletResponse res) throws IOException {
+
+    }
+
+    public static void sendHTMLF3(HttpServletResponse res) throws IOException {
+
     }
 }
